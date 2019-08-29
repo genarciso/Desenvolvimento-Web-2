d0a880ec0e68ab9f4c49e0a826d53557c73b2569
Gabriel Estevam Narciso <20170074318@A307-31L.imd.local>
2019-08-29 14:30:48 -0300

    Continuação da atividade de material

--- Material/src/br/ufrn/imd/material/filtros/SegurancaFilter.java
+++ Material/src/br/ufrn/imd/material/filtros/SegurancaFilter.java
@@ -2,6 +2,7 @@ package br.ufrn.imd.material.filtros;
 
 import java.io.IOException;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import br.ufrn.imd.material.dominio.Usuario;
 
 @WebFilter("/pages/*")
-public class SegurancaFilter {
+public class SegurancaFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 		throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
