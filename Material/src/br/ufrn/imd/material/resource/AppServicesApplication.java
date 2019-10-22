23661a45d5216cb7e521e377e574916e5d54e47d
Gabriel Estevam Narciso <gabriel.estevam.narciso@gmail.com>
2019-10-22 14:10:55 -0300

    Criação da camada de dados do material (#2)

---
+++ Material/src/br/ufrn/imd/material/resource/AppServicesApplication.java
@@ -0,0 +1,9 @@
+package br.ufrn.imd.material.resource;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class AppServicesApplication extends Application{
+	
+}
